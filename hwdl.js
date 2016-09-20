d6f413af459bc647f8b44a009e112adaeefc1225
John Knickerbocker <PokeMaster843@gmail.com>
2016-09-20 07:47:19 -0400

    Update hwdl.js

--- hwdl.js
+++ hwdl.js
@@ -1,1 +1,19 @@
-var index = 
+var canvas = document.getElementById("mainCanvas");
+var ctx = canvas.getContext("2d");
+
+function read() {
+  
+}
+
+function lookAt(obj) {
+  
+}
+
+function predictCategory() {
+  
+}
+
+function confirm(lvl) {
+  
+}
+
