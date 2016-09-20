4daa38eba9349a75f90f643545f56c49625a3803
John Knickerbocker <PokeMaster843@gmail.com>
2016-09-20 12:16:35 -0400

    Update hwdl.js

--- hwdl.js
+++ hwdl.js
@@ -1,19 +1,57 @@
-var canvas = document.getElementById("mainCanvas");
-var ctx = canvas.getContext("2d");
-
-function read() {
+var HWDL = function(mem) {
+  
+  var canvas = document.getElementById("mainCanvas");
+  var ctx = canvas.getContext("2d");
+  var lvl1 = [], lvl2 = [], lvl3 = [], nomatch = [];
+  var lastPair = [];
+  
+  this.read = function(xStart, yStart, xEnd, yEnd) {
+    
+  }
+  
+  this.lookAt = function(obj) {
+    
+    if(lastPair.length >= 2) {
+      
+      lastPair = [];
+      lastPair.push(obj);
+      
+    }
+    
+    else {
+      lastPair.push(obj);
+    }
+    
+  }
+  
+  this.getLastPair = function() {
+    return lastPair;
+  }
+  
+  this.compare = function() {
+    
+  }
+  
+  this.predictCategory = function() {
+    
+  }
+  
+  this.confirm = function(lvl) {
+    lvl1.push([this.getLastPair(), lvl]);
+  }
+  
+  this.nomatch = function() {
+    nomatch.push(this.getLastPair());
+  }
+  
+  this.storeMem = function() {
+    
+    if(true) {
+      
+    }
+    
+    var memFile = new File("memory.txt");
+    
+  }
   
 }
-
-function lookAt(obj) {
-  
-}
-
-function predictCategory() {
-  
-}
-
-function confirm(lvl) {
-  
-}
-
