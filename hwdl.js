f2c9697e708573a13db0f2a63afda2eb256e1670
John Knickerbocker <PokeMaster843@gmail.com>
2016-09-21 10:17:08 -0400

    Update hwdl.js

--- hwdl.js
+++ hwdl.js
@@ -7,45 +7,63 @@ var HWDL = function(mem) {
   
   this.read = function(xStart, yStart, xEnd, yEnd) {
     
-  }
-  
-  this.lookAt = function(obj) {
-    
-    if(lastPair.length >= 2) {
-      
-      lastPair = [];
-      lastPair.push(obj);
-      
-    }
-    
-    else {
-      lastPair.push(obj);
-    }
-    
-  }
+  };
   
   this.getLastPair = function() {
     return lastPair;
-  }
+  };
   
-  this.compare = function() {
+  this.contrastImg = function(imgData, contrast) {
     
-  }
+    var data = imageData.data;
+    var factor = (259 * (contrast + 255)) / (255 * (259 - contrast));
+    
+    for(var i=0; i < data.length; i += 4) {
+      
+      data[i] = factor * (data[i] - 128) + 128;
+      data[i + 1] = factor * (data[i + 1] - 128) + 128;
+      data[i + 2] = factor * (data[i + 2] - 128) + 128;
+      
+    }
+    
+    imageData.data = data;
+    
+  };
+  
+  this.compare = function(obj0, obj1) {
+    
+    lastPair.length = 0;
+    lastPair[0] = obj0;
+    lastPair[1] = obj1;
+    
+    canvas.width = obj0.width + obj1.width;
+    canvas.height = (obj0.height > obj1.height) ? obj0.height : obj1.height;
+    
+    canvas.drawImage(obj0, 0, 0);
+    canvas.drawImage(obj1, obj0.width, 0);
+    
+    var obj0Data = canvas.getImageData(0, 0, obj0.width, obj0.height);
+    var obj1Data = canvas.getImageData(obj.width, 0, obj0.width + obj1.width, obj1.height);
+    
+    this.contrastImg(obj0Data, 30);
+    this.contrastImg(obj1Data, 30);
+    
+  };
   
   this.predictCategory = function() {
     
-  }
+  };
   
   this.confirm = function(lvl) {
     lvl1.push([this.getLastPair(), lvl]);
-  }
+  };
   
   this.nomatch = function() {
     nomatch.push(this.getLastPair());
-  }
+  };
   
   this.storeMem = function() {
     
-  }
+  };
   
-}
+};
