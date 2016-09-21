02c254b631bf84a8594587c46269e24981ffbfe1
John Knickerbocker <PokeMaster843@gmail.com>
2016-09-21 11:40:52 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-var hdwl = new HDWL();
+var mem = "";
+var hdwl = new HDWL(mem);
+var img0 = document.createElement("img"), img1 = document.createElement("img");
+img0.src = "";
+img1.src = "";
+
+hdwl.compare(img0, img1);
