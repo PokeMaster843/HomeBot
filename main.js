6c542c0bb90ad0c7d6d75d892e4822c5e2fddc4e
John Knickerbocker <PokeMaster843@gmail.com>
2016-09-22 07:33:50 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 var mem = "";
-var hdwl = new HDWL(mem);
+//var hdwl = new HDWL(mem);
+var canvas = document.getElementById("mainCanvas");
+var ctx = canvas.getContext("2d");
 var img0 = document.createElement("img"), img1 = document.createElement("img");
-img0.src = "";
-img1.src = "";
+img0.src = "img0.png";
+img0.width = 960;
+img0.height = 480;
+img1.src = "img1.png";
+img1.width = "960";
+img1.height = "480";
 
-hdwl.compare(img0, img1);
+canvas.drawImage(img0, 0, 0);
+canvas.drawImage(img1, img0.width, 0);
+
+//hdwl.compare(img0, img1);
