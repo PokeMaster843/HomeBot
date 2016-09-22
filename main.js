d8d5db9c4af8fb43ba834e5fb9bf133b2956e9e9
John Knickerbocker <PokeMaster843@gmail.com>
2016-09-22 10:24:25 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,5 @@
 var mem = "";
-//var hdwl = new HDWL(mem);
+var hdwl = new HDWL(mem);
 var files = document.getElementById("fileForm");
-var canvas = document.getElementById("mainCanvas");
-var ctx = canvas.getContext("2d");
 
-var update = setInterval(function() {
-  
-  if(files != null) {
-    ctx.drawImage(files.elements[0], 0, 0);
-  }
-  
-}, 1000/60);
-
-//hdwl.compare(files.elements);
+hdwl.compare(files.elements);
