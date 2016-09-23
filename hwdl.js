2e73827962e3f7384c8ccbfbc5eafcc948eca193
John Knickerbocker <PokeMaster843@gmail.com>
2016-09-23 12:19:02 -0400

    Update hwdl.js

--- hwdl.js
+++ hwdl.js
@@ -97,3 +97,21 @@ var HWDL = function(mem) {
   };
   
 };
+
+var mem = "";
+var hdwl = new HDWL(mem);
+
+var img = document.createElement("img");
+img.src = "img0.png";
+document.getElementById("body").appendChild(img);
+
+/*var update = setInterval(function() {
+  
+  if(files != null) {
+    
+    //hdwl.compare(files.elements);
+    document.getElementById("mainCanvas").getContext("2d").drawImage(files.elements[0], 0, 0);
+    
+  }
+  
+}, 1000 / 60);*/
